c33f21880c7e56179626cfdfe5a62a670bc83267
Vini <126912155+vinniscript@users.noreply.github.com>
2023-03-16 21:47:14 -0300

    Atividades

---
+++ Exemplo04.java
@@ -0,0 +1,28 @@
+package aula1603;
+
+import java.util.Scanner;
+
+public class Exemplo04 {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.printf("Manda um número aí: ");
+
+        int numero = kb.nextInt();
+
+        if (numero % 2 == 0) {
+            if (numero > 0) {
+                System.out.println("Numero par e positivo.");
+            } else {
+                System.out.println("Numero par e negativo.");
+            }
+        } else {
+            if (numero > 0) {
+                System.out.println("Numero ímpar e positivo.");
+            } else {
+                System.out.println("Numero ímpar e negativo.");
+            }
+        }
+    }
+}
