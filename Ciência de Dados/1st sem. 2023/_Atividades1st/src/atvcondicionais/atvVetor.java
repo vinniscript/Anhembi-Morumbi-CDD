560c4f18541de7ccdc8b8e52ea02d9c068bb321c
Vinicius Sousa <viniciusps.1190@gmail.com>
2023-04-04 19:08:19 -0300

    Adicionando o while no exercício.

--- Ciência de Dados/1st sem. 2023/_Atividades1st/src/atvcondicionais/atvVetor.java
+++ Ciência de Dados/1st sem. 2023/_Atividades1st/src/atvcondicionais/atvVetor.java
@@ -10,23 +10,37 @@ public class atvVetor {
         Random gerador = new Random();
 
         int[] vetor = new int[50];  // <--
+        int contador = 0;
 
-        System.out.printf("Estou no vetor: \n\n");
+        while (contador <= vetor.length - 1) {
 
-        for (int i = 0; i <= vetor.length- 1; i++) { // <- Para um output mais claro, define um "-1"na frente do length do vetor caso você não queria que ele comece pelo index '0', junto disso...
-            
-// Vai gerar uma sequência de 50 números aleatórios (por causa do vetor.length, que declarei para ser de 50).                                               //
-            vetor[i] += gerador.nextInt();
-                                                                                                                                                //
-            System.out.printf("%d -> %d \n", i + 1, vetor[i]);                                                    
-                                                                                                                                    // é super importante colocar "i + 1", se não ele para no index 49, mesmo com <= declarado.
+            vetor[contador] += gerador.nextInt();
+
+            System.out.printf("%d -> %d\n", contador + 1, vetor[contador]);
+
+            contador++;
 
         }
+    }
+    
+    
+    // FOR --
 
+//        System.out.printf("Estou no vetor: \n\n");
+//
+//        for (int i = 0; i <= vetor.length- 1; i++) { // <- Para um output mais claro, define um "-1"na frente do length do vetor caso você não queria que ele comece pelo index '0', junto disso...
+//            
+//// Vai gerar uma sequência de 50 números aleatórios (por causa do vetor.length, que declarei para ser de 50).                                               //
+//            vetor[i] += gerador.nextInt();
+//                                                                                                                                                //
+//            System.out.printf("%d -> %d \n", i + 1, vetor[i]);                                                    
+//                                                                                                                                    // é super importante colocar "i + 1", se não ele para no index 49, mesmo com <= declarado.
+//
+//        }
 //        System.out.println("\n Sai do vetor \n");
 //
 //        for (int i = 1; i < vetor.length; i++) {
 //            System.out.printf("%d -> %d \n", i, vetor[i]);
 //        }
-    }
 }
+
