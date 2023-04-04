73626d815ed0c02d169c4eecebdcbe59ac194995
Vinicin <viniciusps.1190@gmail.com>
2023-04-03 21:50:49 -0300

    Aula segunda (03/04)

---
+++ Ciência de Dados/1st sem. 2023/_Atividades1st/src/atvcondicionais/atvVetor.java
@@ -0,0 +1,17 @@
+    
+package atvcondicionais;
+
+// Criar números aleatórios e o armazenar dentro de um vetor.
+
+import java.util.Scanner;
+
+
+public class atvVetor {
+    public static void main(String[] args) {
+        int []vetor = new int[50];
+        Scanner kb = new Scanner(System.in); 
+        
+        for(int i = 0; i < vetor.length ; i++ ) {
+        }
+    }
+}
