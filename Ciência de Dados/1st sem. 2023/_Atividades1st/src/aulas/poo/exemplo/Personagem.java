2a129054a1ef5aca7f5b56107efb1d0556be4f29
Vinicius Sousa <viniciusps.1190@gmail.com>
2023-04-24 21:52:19 -0300

    ...

--- Ciência de Dados/1st sem. 2023/_Atividades1st/src/aulas/poo/exemplo/Personagem.java
+++ Ciência de Dados/1st sem. 2023/_Atividades1st/src/aulas/poo/exemplo/Personagem.java
@@ -1,17 +1,27 @@
-
 package aulas.poo.exemplo;
 
-
 public class Personagem {
+
     private int vida;
     private String nome;
+
     // MÉTODO CONSTRUTOR
-    public Personagem(String nome, int vida){
+    public Personagem(String nome, int vida) {
         this.nome = nome.toUpperCase();
         this.vida = vida;
     }
-    
-    public void cumprimentar(){
+
+    public void cumprimentar() {
         System.out.println("Olá, eu sou o " + this.nome);
+    }
+    //Colocando nova vida ao personagem
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    // Pegar valores da vida
+
+    public int getVida() {
+        return this.vida;
     }
 }
